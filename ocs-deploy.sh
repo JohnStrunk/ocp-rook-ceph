b88c0501a6992b65ac42d86a64d840702162ba21
John Strunk <jstrunk@redhat.com>
2019-05-14 17:02:45 -0400

    initial commit
    Signed-off-by: John Strunk <jstrunk@redhat.com>

---
+++ ocs-deploy.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+#--
+# See: https://rook.io/docs/rook/master/ceph-csi-drivers.html
+#--
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR" || exit 1
+
+#-- pull straight from GH. Could also set this to a local copy
+ROOK_PATH="https://raw.githubusercontent.com/rook/rook/master"
+NAMESPACE="rook-ceph"
+REPLICAS=3
+MANIFESTS=(
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/common.yaml"
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/csi/rbac/cephfs/csi-nodeplugin-rbac.yaml"
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/csi/rbac/cephfs/csi-provisioner-rbac.yaml"
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/csi/rbac/rbd/csi-nodeplugin-rbac.yaml"
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/csi/rbac/rbd/csi-provisioner-rbac.yaml"
+            "${ROOK_PATH}/cluster/examples/kubernetes/ceph/operator-openshift-with-csi.yaml"
+            "cluster.yaml"
+            "replicapool.yaml"
+            "mycephfs.yaml"
+            "csi-rbd.yaml"
+            "csi-cephfs.yaml"
+          )
+
+../ocp4 create namespace "$NAMESPACE"
+
+echo Obtaining block devices...
+../ocp4 -n "$NAMESPACE" apply -f block-pv.yml
+while [[ $(../ocp4 -n "$NAMESPACE" get statefulset/block-devs -ojsonpath='{.status.readyReplicas}') != "$REPLICAS" ]]; do
+        echo Waiting for replicas to be ready...
+        sleep 5
+done
+
+for m in ${MANIFESTS[*]}; do
+        ../ocp4 -n "$NAMESPACE" apply -f "$m"
+done
+
+while [[ $(../ocp4 -n "$NAMESPACE" get po --field-selector='status.phase==Running' | grep rook-ceph-osd | grep -cv prepare) != "$REPLICAS" ]]; do
+        echo Waiting for OSDs to be running...
+        sleep 10
+done
+
+#-- retrieve admin key
+OPERATOR_POD="$(../ocp4 -n "$NAMESPACE" get po | grep rook-ceph-operator | awk '{print $1}')"
+ADMIN_KEY="$(../ocp4 -n "$NAMESPACE" exec -it "$OPERATOR_POD" -- ceph auth get-key client.admin)"
+
+#-- Create CSI RBD secret
+../ocp4 -n "$NAMESPACE" create secret generic csi-rbd-secret "--from-literal=admin=$ADMIN_KEY"
+
+#-- Create CSI CephFS secret
+../ocp4 -n "$NAMESPACE" create secret generic csi-cephfs-secret "--from-literal=userID=admin" "--from-literal=userKey=$ADMIN_KEY" "--from-literal=adminID=admin" "--from-literal=adminKey=$ADMIN_KEY"
