59f36ee91bbfafcc9f043a5360d55370b135dd92
John Strunk <jstrunk@redhat.com>
2020-03-17 14:55:13 -0400

    Modifications to deploy on Dedicated
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- reshape-cluster-osd.sh
+++ reshape-cluster-osd.sh
@@ -6,3 +6,9 @@ OC="$(realpath "${OC:-"$(command -v oc)"}")"
 
 # Label all worker nodes
 "$OC" label no --overwrite -lnode-role.kubernetes.io/worker cluster.ocs.openshift.io/openshift-storage=""
+
+# Remove subscription validation webhook
+"$OC" delete validatingwebhookconfigurations/sre-subscription-validation
+
+# Add the upstream community operators
+"$OC" apply -f community-operators.yaml
