63ee680eccc31d7b1dbb282ad0c95340292e2233
John Strunk <jstrunk@redhat.com>
2020-06-19 11:51:56 -0400

    Change how operator catalogs are handled on OSD
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- reshape-cluster-osd.sh
+++ reshape-cluster-osd.sh
@@ -7,8 +7,8 @@ OC="$(realpath "${OC:-"$(command -v oc)"}")"
 # Label all worker nodes
 "$OC" label no --overwrite -lnode-role.kubernetes.io/worker cluster.ocs.openshift.io/openshift-storage=""
 
-# Remove subscription validation webhook
-"$OC" delete validatingwebhookconfigurations/sre-subscription-validation
-
 # Add the upstream community operators
-"$OC" apply -f community-operators.yaml
+#"$OC" apply -f community-operators.yaml
+"$OC" -n openshift-marketplace patch operatorsource/osd-curated-community-operators --type merge -p '{"spec":{"registryNamespace": "community-operators"}}'
+# Add full RH operators (for OCS)
+"$OC" -n openshift-marketplace patch operatorsource/osd-curated-redhat-operators --type merge -p '{"spec":{"registryNamespace": "redhat-operators"}}'
